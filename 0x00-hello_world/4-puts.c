78151b11cf994011c5dfd9893fd7a83c877801c7
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-15 10:41:05 -0700

    C intro

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include "stdio.h"
+
 /**
- *main - entry point
- *return: always 0
- */
+* main - Entry point
+*
+* Description: 'the programs description'
+*
+* Return: always 0 (success)
+*/
 int	main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
