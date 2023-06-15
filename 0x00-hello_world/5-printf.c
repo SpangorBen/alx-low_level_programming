b600457c27e594ad7ec4d74afcfa2d47902ff8d6
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-15 10:50:38 -0700

    C programming

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* Description: 'the programs description'
+*
+* Return: always 0 (success)
+*/
+int	main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
