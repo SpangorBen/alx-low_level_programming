0fe17e85c86f0dba6f873745064fa74b0b57edcb
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-15 11:28:24 -0700

    C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -9,6 +9,7 @@
 */
 int	main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	const char* message = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(2, message, 60);
 	return (0);
 }
