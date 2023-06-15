474e79c7d211cf264a1290e7bc60c914fc375397
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-15 11:18:31 -0700

    C programming

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,7 +1,14 @@
 #include <unistd.h>
 
+/**
+* main - Entry point
+*
+* Description: 'the programs description'
+*
+* Return: always 0 (success)
+*/
 int	main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",59);
 	return (0);	
 }
