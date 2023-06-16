f8b6acd6c2ee6fe923393282f60d0bcfdedfbbbd
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-16 13:27:31 +0100

    print numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - print numbers
+ *
+ * Return: always 0
+*/
+
+int	main(void)
+{
+	int n = 48;
+
+	while (n < 58)
+	{
+		putchar(n);
+		n++;
+	}
+	putchar('\n');
+	return (0);
+}
