e2412743763202f16e17273fbd627a88c369fdae
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-20 19:10:23 +0100

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - last digit
+ * @n: check value
+ * Return: last digit of n
+*/
+
+int print_last_digit(int n)
+{
+	int l;
+
+	l = n % 10;
+	return (l);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -6,6 +6,7 @@ int 	_abs(int n);
 int		_islower(int c);
 int		_isalpha(int c);
 int		_putchar(char c);
+int 	print_last_digit(int n);
 int		print_sign(int n);
 void	print_alphabet(void);
 void	print_alphabet_x10(void);
