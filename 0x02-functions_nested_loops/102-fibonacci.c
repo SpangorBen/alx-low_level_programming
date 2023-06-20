ce78a456374c276a9db93f3707822a01099a60f8
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-21 00:55:01 +0100

    fibonacci

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * main - fibonacci
+ * Return: 0
+*/
+
+int	main(void)
+{
+	long n = 1;
+	long m = 2;
+	long r;
+	int i = 0;
+
+	while (i <= 50)
+	{
+		r = n + m;
+		printf("%ld", n);
+		if (i < 50)
+			printf(", ");
+		n = m;
+		m = r;
+		i++;
+	}
+	return (0);
+}
