96be951885cc78b432b21b0c9bda82cb82609322
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-20 20:31:16 +0100

    to 98 we go

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_to_98 - print to 98
+ * @n: number to count from
+ * Return: anything
+*/
+
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n++;
+		}
+	}
+	else
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+				printf(", ");
+			n--;
+		}
+	}
+	printf("\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -11,6 +11,7 @@ int		print_sign(int n);
 int 	print_last_digit(int n);
 void 	jack_bauer(void);
 void 	times_table(void);
+void 	print_to_98(int n);
 void	print_alphabet(void);
 void	print_alphabet_x10(void);
 
