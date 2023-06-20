bb114ed0f1c83a36b9950dfa1892286ef8031d8e
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-20 18:23:37 +0100

    print 10times

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - print 10 times
+ * Return: void
+*/
+
+void	print_alphabet_x10(void)
+{
+	int	i = 0;
+	int c = 'a';
+
+	while (i < 10)
+	{
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,7 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
-void	print_alphabet(void);
 int		_putchar(char c);
+void	print_alphabet(void);
+void	print_alphabet_x10(void);
 
 #endif
