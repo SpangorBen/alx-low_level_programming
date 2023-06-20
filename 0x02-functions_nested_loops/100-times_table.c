2c6f3032493f7179f8f59261c08c752d84463419
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-21 00:02:57 +0100

    print table

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * print_times_table - print table
+ * @n: table size
+ * Return: none
+*/
+
+void	print_times_table(int n)
+{
+	int i = 0, j, r, m;
+
+	if (n < 15 && n > 0)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				r = j * i;
+				if (j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+				}
+				if (r > 9 && r < 100)
+				{
+					_putchar(' ');
+					_putchar('0' + r / 10);
+					_putchar('0' + r % 10);
+				}
+				else if (r > 99)
+				{
+					_putchar('0' + r / 100);
+					_putchar('0' + (r / 10) % 10);
+					_putchar('0' + r % 10);
+				}
+				else if (r <= 9 && j != 0)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar('0' + r);
+				}
+				else
+					_putchar('0' + r);
+			}
+			_putchar('\n');
+		}
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -13,6 +13,7 @@ void 	jack_bauer(void);
 void 	times_table(void);
 void 	print_to_98(int n);
 void	print_alphabet(void);
+void 	print_times_table(int n);
 void	print_alphabet_x10(void);
 
 #endif
