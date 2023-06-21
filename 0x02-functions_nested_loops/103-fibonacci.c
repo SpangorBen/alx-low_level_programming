f094a96d8d3549394859ea9cda5ccfef90f629e6
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-21 01:04:41 +0100

    fibonacci sum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+int	main(void)
+{
+	int	i = 0;
+	long r;
+	long sum;
+	long m = 2, n = 1;
+	while (i < 50)
+	{
+		r = n + m;
+		n = m;
+		m = r;
+		if (n % 2 == 0 && n <= 4000000)
+			sum += n;
+		i++;
+	}
+	printf("%ld\n", n);
+	return (0);
+}
