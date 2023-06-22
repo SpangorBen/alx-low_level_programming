9f180c36070c342a86d7ba4989f15595670af9d1
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-23 00:14:16 +0100

    factor

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - prime factor
+ * Return: 0
+*/
+
+int main(void)
+{
+	long int i = 2;
+	long int p;
+	long int n = 612852475143;
+
+	while (i <= n)
+	{
+		if (n % i == 0)
+		{
+			n = n / i;
+			p = i;
+		}
+		i++;
+	}
+	printf("%ld\n", p);
+}
---
+++ 0x04-more_functions_nested_loops/test.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	long i;
+	long num = 612852475143;
+
+	for (i = 2; i < num; i++)
+	{
+		if (num % i == 0)
+			num /= i;
+	}
+	printf("%li\n", num);
+	return (0);
+}
