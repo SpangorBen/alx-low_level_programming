0b4aa5e74ac434d2069413a7aad1fe1fe5c08266
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-22 23:01:24 +0100

    more nums

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * more_numbers - print nums 10 times
+ * Return: none
+*/
+
+void more_numbers(void)
+{
+	int i = 0, j;
+
+	while (i < 10)
+	{
+		j = 0;
+		while (j < 15)
+		{
+			if (j > 9)
+				_putchar(j / 10 + '0');
+			_putchar(j % 10 + '0');
+			j++;
+		}
+		i++;
+		_putchar('\n');
+	}
+}
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -5,7 +5,8 @@ int 	_isupper(int c);
 int 	_isdigit(int c);
 int 	_putchar(char c);
 int 	mul(int a, int b);
-void	print_most_numbers(void);
+void	more_numbers(void);
 void	print_numbers(void);
+void	print_most_numbers(void);
 
 #endif
