a85af6e8b003b299fccb85e0bdae48bcc46382df
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-22 23:13:54 +0100

    diag

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_diagonal - diagonal
+ * @n: number
+ * Return: none
+*/
+
+void print_diagonal(int n)
+{
+	int i = 0, j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+	while (i < n)
+	{
+		j = 1;
+		while (j < i)
+		{
+			if (i != 1)
+				_putchar(' ');
+			if (i == j)
+			{
+				_putchar('\\');
+				_putchar('\n');
+			}
+			j++;
+		}
+		i++;
+	}
+}
