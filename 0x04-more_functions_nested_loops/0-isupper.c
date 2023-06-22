350fb09d667b76d1cb1ce0dc94a716b314800ce8
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-22 12:50:45 +0100

    upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - check uppercase
+ * @c: character to check
+ * Return: 1 if its true and 0 if false
+*/
+
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
+}
