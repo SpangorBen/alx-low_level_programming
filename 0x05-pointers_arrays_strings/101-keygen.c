97e474d243af875638d4df466305d7f57a3158ab
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-27 03:07:35 +0100

    keygen

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <time.h>
+
+/**
+ * main - main program
+ * Return: 0
+*/
+
+int main(void)
+{
+	int r = 0, i = 0;
+	time_t t;
+
+	srand((unsigned int) time(&t));
+	while (i < 2772)
+	{
+		r = rand() % 128;
+		if ((i + r) > 2772)
+			break;
+		i = i + r;
+		printf("%c", r);
+	}
+	printf("%c\n", (2772 - i));
+	return (0);
+}
