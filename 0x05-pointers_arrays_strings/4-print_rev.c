7acfa972ecd9a979e9acc70b0ac8bec294634b4c
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-27 02:09:45 +0100

    rev

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_rev - rev of str
+ * @s: string to rev
+ * Return: none
+*/
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+		i++;
+	i--;
+	while (i > 0)
+	{
+		_putchar(s[i]);
+		i--;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -4,6 +4,7 @@
 int 	_putchar(char c);
 int 	_strlen(char *s);
 void	_puts(char *str);
+void	print_rev(char *s);
 void	reset_to_98(int *n);
 void	swap_int(int *a, int *b);
 
