dd632f815a782c3ef1d2a2b4a369adab5c87045f
SpangorBen <netflixbenzzinbi@gmail.com>
2023-06-30 01:45:51 +0100

    string

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * *leet - Entry point
+ * Description: Encodes a string into 1337
+ * @str: String
+ * Return: String
+ */
+
+char *leet(char *str)
+{
+	char *enc = str;
+	char *letter = "aAeEoOtTlL";
+	char *num = "4433007711";
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (str[i] == letter[j])
+			{
+				enc[i] = num[j];
+				break;
+			}
+		}
+	}
+	return (enc);
+}
