210c806054d8358c8a72142f63b19b7ce9bef99c
SpangorBen <netflixbenzzinbi@gmail.com>
2023-07-03 23:39:29 +0100

    fixed

--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -9,6 +9,7 @@ char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 void print_chessboard(char (*a)[8]);
 void print_diagsums(int *a, int size);
+void set_string(char **s, char *to);
 char 	*_memset(char *s, char b, unsigned int n);
 char 	*_memcpy(char *dest, char *src, unsigned int n);
 
