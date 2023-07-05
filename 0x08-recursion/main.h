2e02f1ba01267853d18a7e9c280503071cc187d6
SpangorBen <netflixbenzzinbi@gmail.com>
2023-07-05 12:21:38 +0100

    palindrome

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,7 @@ int 	factorial(int n);
 int 	_putchar(char c);
 int 	_sqrt_recursion(int n);
 int 	is_prime_number(int n);
+int 	is_palindrome(char *s);
 int 	_strlen_recursion(char *s);
 int 	_pow_recursion(int x, int y);
 void 	_puts_recursion(char *s);
