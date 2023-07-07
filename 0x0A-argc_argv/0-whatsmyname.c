e41ae69fdca4832d5dc9caf173997edb630f0ed5
SpangorBen <netflixbenzzinbi@gmail.com>
2023-07-08 00:03:34 +0100

    ac av

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - main
+ * @ac: Number of command line arguments
+ * @av: arrays
+ * Return: always 0
+*/
+
+int main(int ac, char **av)
+{
+	printf("%s\n", av[0]);
+	return (0);
+}
