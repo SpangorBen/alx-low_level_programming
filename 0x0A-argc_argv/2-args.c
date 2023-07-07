404f63a842a86b8383e8420ef379784d8e02ffba
SpangorBen <netflixbenzzinbi@gmail.com>
2023-07-08 00:16:36 +0100

    args

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - main
+ * @ac: nbr of args
+ * @av: args
+ * Return: 0
+*/
+
+int main(int ac, char **av)
+{
+	int i = 0;
+	
+	while (i < ac)
+	{
+		printf("%s\n", av[i]);
+		i++;
+	}
+	return (0);
+}
