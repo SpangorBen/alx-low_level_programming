69d0ea2536bf19c6a5241a1732c6902a4cfbce7e
SpangorBen <netflixbenzzinbi@gmail.com>
2023-07-11 15:25:35 +0100

    strdup

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strdup - duplicate
+ * @str: string
+ * Return: new duplicated string
+*/
+
+char *_strdup(char *str)
+{
+	int i = 0;
+	int l = 1;
+	char *dup;
+
+	if (!str)
+		return (NULL);
+	while (str)
+		l++;
+	dup = malloc(l);
+	while (str)
+	{
+		dup[i] = str[i];
+		i++;
+	}
+	dup[i] = '\0';
+	return (dup);
+}
--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdlib.h>
+char *_strdup(char *str);
 char *create_array(unsigned int size, char c);
 
 #endif
